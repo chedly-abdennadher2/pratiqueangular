a04eeaaf8c7c892e40d4f9dca06a7e065006a8dc
chedly-abdennadher2 <chedly.abdennadher@enicar.ucar.tn>
2023-08-31 19:39:06 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponentModule } from './accueil-component/accueil-component.module';
+import { CreateComponent } from './accueil-component/create/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accueil', component: CreateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
